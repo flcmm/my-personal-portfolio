a0c410e48c94305ab3375fa18e518a4eb05a3274
flcmm <froilanaquino1@gmail.com>
2023-02-22 16:24:46 +0800

    added new secctions and link for footer

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
   return (
     <div className="md:hidden flex flex-row gap-x-5 justify-between items-center">
-      <Link to='/services'>
-        <h1 className='dark:text-slate-300 text-xs font-medium'> Services</h1>
+      <Link to='/'>
+        <h1 className='dark:text-slate-300 text-xs font-medium'>About</h1>
       </Link>
-      <Link to='notes'>
-        <h1 className='dark:text-slate-300 text-xs font-medium'> Notes</h1>
+      <Link to='expertise'>
+        <h1 className='dark:text-slate-300 text-xs font-medium'>Expertise</h1>
       </Link>
-      <Link to='works'>
-        <h1 className='dark:text-slate-300 text-xs font-medium'> Works</h1>
+      <Link to='projects'>
+        <h1 className='dark:text-slate-300 text-xs font-medium'>Projects</h1>
       </Link>
       <Link to='contact'>
         <h1 className='dark:text-slate-300 text-xs font-medium'>Contact</h1>
