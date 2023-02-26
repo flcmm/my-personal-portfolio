8a7211eb0c7df0f4e8017ed27924e52e7b42784a
flcmm <froilanaquino1@gmail.com>
2023-02-26 21:24:17 +0800

    added contact page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="">
-      <h1 className='font-bold text-black dark:text-white'>About Me</h1>
-      <div className="">
-        <h1 className="text-black dark:xtext-white">Hello there, I'm Froilan</h1>
-        <p>I am a web developer and user interface designer based on Caloocan City Philippines. I also do database management system using MySQL. I am currently getting my bachelor's degree for information technology field in my college school St. Vincent De Ferrer College</p>
+    <div className="p-5">
+      <h1 className='font-black text-black text-2xl dark:text-white'>About Me</h1>
+      <div className="pt-5">
+        <h1 className="text-black dark:text-white">Hello there, I'm Froilan</h1>
+        <p className='text-black dark:text-white font-medium'>I am a junior web developer and user interface designer based on Caloocan City Philippines. I also do database management system using MySQL. I am currently getting my bachelor's degree for information technology field in my college school St. Vincent De Ferrer College. </p>
       </div>
     </div>
   )
